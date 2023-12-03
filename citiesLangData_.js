9309f5261b984c73c13f5ce7b45fabbf0cea4bc1
artdian <46709199+jartdian@users.noreply.github.com>
2023-12-03 22:46:39 +0100

    initial commit

---
+++ citiesLangData_.js
@@ -0,0 +1,379 @@
+const citiesLangData = {
+  "Rotterdam": {
+    "coordinates": [
+      51.9225,
+      4.47917
+    ],
+    "data": {
+      "javascript": 655,
+      "python": 688,
+      "ruby": 29,
+      "java": 560,
+      "C%2B%2B": 246,
+      "c%23": 709,
+      "go": 1215,
+      "swift": 71,
+      "php": 564,
+      "sql": 975,
+      "typescript": 253,
+      "kotlin": 71
+    }
+  },
+  "Amsterdam": {
+    "coordinates": [
+      52.3676,
+      4.9041
+    ],
+    "data": {
+      "javascript": 710,
+      "python": 897,
+      "ruby": 72,
+      "java": 995,
+      "C%2B%2B": 264,
+      "c%23": 569,
+      "go": 2609,
+      "swift": 139,
+      "php": 426,
+      "sql": 1176,
+      "typescript": 488,
+      "kotlin": 236
+    }
+  },
+  "Den Haag": {
+    "coordinates": [
+      52.0705,
+      4.3007
+    ],
+    "data": {
+      "javascript": 604,
+      "python": 658,
+      "ruby": 31,
+      "java": 507,
+      "C%2B%2B": 200,
+      "c%23": 615,
+      "go": 1200,
+      "swift": 72,
+      "php": 526,
+      "sql": 886,
+      "typescript": 237,
+      "kotlin": 66
+    }
+  },
+  "Utrecht": {
+    "coordinates": [
+      52.0907,
+      5.1214
+    ],
+    "data": {
+      "javascript": 642,
+      "python": 570,
+      "ruby": 22,
+      "java": 809,
+      "C%2B%2B": 121,
+      "c%23": 552,
+      "go": 802,
+      "swift": 45,
+      "php": 406,
+      "sql": 972,
+      "typescript": 327,
+      "kotlin": 187
+    }
+  },
+  "Eindhoven": {
+    "coordinates": [
+      51.4416,
+      5.4697
+    ],
+    "data": {
+      "javascript": 244,
+      "python": 257,
+      "ruby": 15,
+      "java": 201,
+      "C%2B%2B": 286,
+      "c%23": 335,
+      "go": 739,
+      "swift": 41,
+      "php": 167,
+      "sql": 373,
+      "typescript": 72,
+      "kotlin": 28
+    }
+  },
+  "Groningen": {
+    "coordinates": [
+      53.2194,
+      6.5665
+    ],
+    "data": {
+      "javascript": 103,
+      "python": 57,
+      "ruby": 13,
+      "java": 81,
+      "C%2B%2B": 26,
+      "c%23": 57,
+      "go": 83,
+      "swift": 6,
+      "php": 55,
+      "sql": 76,
+      "typescript": 39,
+      "kotlin": 6
+    }
+  },
+  "Zwolle": {
+    "coordinates": [
+      52.515,
+      6.0835
+    ],
+    "data": {
+      "javascript": 99,
+      "python": 50,
+      "ruby": 1,
+      "java": 51,
+      "C%2B%2B": 39,
+      "c%23": 97,
+      "go": 95,
+      "swift": 3,
+      "php": 77,
+      "sql": 132,
+      "typescript": 31,
+      "kotlin": 6
+    }
+  },
+  "Nijmegen": {
+    "coordinates": [
+      51.8426,
+      5.8546
+    ],
+    "data": {
+      "javascript": 165,
+      "python": 106,
+      "ruby": 2,
+      "java": 104,
+      "C%2B%2B": 64,
+      "c%23": 185,
+      "go": 285,
+      "swift": 22,
+      "php": 112,
+      "sql": 200,
+      "typescript": 69,
+      "kotlin": 9
+    }
+  },
+  "Tilburg": {
+    "coordinates": [
+      51.5719,
+      5.0672
+    ],
+    "data": {
+      "javascript": 283,
+      "python": 169,
+      "ruby": 10,
+      "java": 166,
+      "C%2B%2B": 81,
+      "c%23": 344,
+      "go": 571,
+      "swift": 31,
+      "php": 251,
+      "sql": 484,
+      "typescript": 99,
+      "kotlin": 27
+    }
+  },
+  "'s-Hertogenbosch": {
+    "coordinates": [
+      51.6888,
+      5.2884
+    ],
+    "data": {
+      "javascript": 258,
+      "python": 170,
+      "ruby": 20,
+      "java": 166,
+      "C%2B%2B": 89,
+      "c%23": 339,
+      "go": 566,
+      "swift": 29,
+      "php": 212,
+      "sql": 456,
+      "typescript": 95,
+      "kotlin": 31
+    }
+  },
+  "Breda": {
+    "coordinates": [
+      51.5831,
+      4.7484
+    ],
+    "data": {
+      "javascript": 153,
+      "python": 93,
+      "ruby": 2,
+      "java": 68,
+      "C%2B%2B": 44,
+      "c%23": 201,
+      "go": 430,
+      "swift": 13,
+      "php": 184,
+      "sql": 280,
+      "typescript": 42,
+      "kotlin": 10
+    }
+  },
+  "Almere": {
+    "coordinates": [
+      52.3508,
+      5.2647
+    ],
+    "data": {
+      "javascript": 559,
+      "python": 704,
+      "ruby": 70,
+      "java": 832,
+      "C%2B%2B": 227,
+      "c%23": 474,
+      "go": 2231,
+      "swift": 106,
+      "php": 338,
+      "sql": 927,
+      "typescript": 432,
+      "kotlin": 207
+    }
+  },
+  "Apeldoorn": {
+    "coordinates": [
+      52.2112,
+      5.9699
+    ],
+    "data": {
+      "javascript": 82,
+      "python": 75,
+      "ruby": 2,
+      "java": 151,
+      "C%2B%2B": 39,
+      "c%23": 86,
+      "go": 116,
+      "swift": 8,
+      "php": 64,
+      "sql": 149,
+      "typescript": 35,
+      "kotlin": 13
+    }
+  },
+  "Arnhem": {
+    "coordinates": [
+      51.9851,
+      5.8987
+    ],
+    "data": {
+      "javascript": 210,
+      "python": 125,
+      "ruby": 3,
+      "java": 126,
+      "C%2B%2B": 76,
+      "c%23": 234,
+      "go": 277,
+      "swift": 22,
+      "php": 124,
+      "sql": 272,
+      "typescript": 95,
+      "kotlin": 16
+    }
+  },
+  "Haarlem": {
+    "coordinates": [
+      52.3874,
+      4.9024
+    ],
+    "data": {
+      "javascript": 653,
+      "python": 817,
+      "ruby": 67,
+      "java": 916,
+      "C%2B%2B": 252,
+      "c%23": 499,
+      "go": 2506,
+      "swift": 129,
+      "php": 391,
+      "sql": 1064,
+      "typescript": 457,
+      "kotlin": 223
+    }
+  },
+  "Enschede": {
+    "coordinates": [
+      52.2215,
+      6.8937
+    ],
+    "data": {
+      "javascript": 93,
+      "python": 73,
+      "ruby": 13,
+      "java": 140,
+      "C%2B%2B": 92,
+      "c%23": 93,
+      "go": 112,
+      "swift": 23,
+      "php": 101,
+      "sql": 110,
+      "typescript": 31,
+      "kotlin": 13
+    }
+  },
+  "Amersfoort": {
+    "coordinates": [
+      52.1561,
+      5.3878
+    ],
+    "data": {
+      "javascript": 644,
+      "python": 577,
+      "ruby": 22,
+      "java": 741,
+      "C%2B%2B": 136,
+      "c%23": 569,
+      "go": 921,
+      "swift": 44,
+      "php": 437,
+      "sql": 973,
+      "typescript": 351,
+      "kotlin": 186
+    }
+  },
+  "Leeuwarden": {
+    "coordinates": [
+      53.2194,
+      5.8054
+    ],
+    "data": {
+      "javascript": 24,
+      "python": 5,
+      "java": 18,
+      "C%2B%2B": 7,
+      "c%23": 28,
+      "go": 42,
+      "php": 24,
+      "sql": 19,
+      "typescript": 8,
+      "kotlin": 1
+    }
+  },
+  "Sittard": {
+    "coordinates": [
+      51.0018,
+      5.8662
+    ],
+    "data": {
+      "javascript": 64,
+      "python": 33,
+      "java": 33,
+      "C%2B%2B": 13,
+      "c%23": 47,
+      "go": 189,
+      "swift": 7,
+      "php": 47,
+      "sql": 56,
+      "typescript": 10,
+      "kotlin": 8
+    }
+  }
+};
---
+++ citiesLangScraplenium.py
@@ -0,0 +1,115 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+import json
+import time
+import random
+
+languages = [
+    "javascript",
+    "python",
+    "ruby",
+    "java",
+    "C%2B%2B",
+    "c%23",
+    "go",
+    "swift",
+    "php",
+    "sql",
+    "typescript",
+    "kotlin",
+]
+cities = [
+    "Amsterdam",
+    "Rotterdam",
+    "Den Haag",
+    "Utrecht",
+    "Eindhoven",
+    "Groningen",
+    "Zwolle",
+    "Nijmegen",
+    "Tilburg",
+    "'s-Hertogenbosch",
+    "Breda",
+    "Almere",
+    "Apeldoorn",
+    "Arnhem",
+    "Haarlem",
+    "Enschede",
+    "Amersfoort",
+    "Leeuwarden",
+    "Sittard",
+]
+
+citiesLangData = {
+    "Rotterdam": {"coordinates": [51.9225, 4.47917], "data": {}},
+    "Amsterdam": {"coordinates": [52.3676, 4.9041], "data": {}},
+    "Den Haag": {"coordinates": [52.0705, 4.3007], "data": {}},
+    "Utrecht": {"coordinates": [52.0907, 5.1214], "data": {}},
+    "Eindhoven": {"coordinates": [51.4416, 5.4697], "data": {}},
+    "Groningen": {"coordinates": [53.2194, 6.5665], "data": {}},
+    "Zwolle": {"coordinates": [52.5150, 6.0835], "data": {}},
+    "Nijmegen": {"coordinates": [51.8426, 5.8546], "data": {}},
+    "Tilburg": {"coordinates": [51.5719, 5.0672], "data": {}},
+    "'s-Hertogenbosch": {"coordinates": [51.6888, 5.2884], "data": {}},
+    "Breda": {"coordinates": [51.5831, 4.7484], "data": {}},
+    "Almere": {"coordinates": [52.3508, 5.2647], "data": {}},
+    "Apeldoorn": {"coordinates": [52.2112, 5.9699], "data": {}},
+    "Arnhem": {"coordinates": [51.9851, 5.8987], "data": {}},
+    "Haarlem": {"coordinates": [52.3874, 4.9024], "data": {}},
+    "Enschede": {"coordinates": [52.2215, 6.8937], "data": {}},
+    "Amersfoort": {"coordinates": [52.1561, 5.3878], "data": {}},
+    "Leeuwarden": {"coordinates": [53.2194, 5.8054], "data": {}},
+    "Sittard": {"coordinates": [51.0018, 5.8662], "data": {}},
+}
+
+url_base = "https://nl.indeed.com/jobs?q={}&l={}&radius=25"
+
+driver = webdriver.Chrome()
+
+# Counter to keep track of successful iterations
+successful_iterations = 0
+
+# Start loop from where you left off or 0 if starting fresh
+start_iteration = 0
+
+try:
+    for iteration, language in enumerate(languages, start=start_iteration):
+        for city in cities:
+            url = url_base.format(language, city)
+            driver.get(url)
+            sleep_time = random.uniform(4, 7)
+            time.sleep(sleep_time)
+
+            try:
+                element_to_capture = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, ".jobsearch-JobCountAndSortPane-jobCount"))
+                )
+                element_text = element_to_capture.text
+                job_count = int("".join(filter(str.isdigit, element_text)))
+
+                citiesLangData[city]["data"][language] = job_count
+                successful_iterations += 1
+
+                # Your code to write to a file here (adjust as needed)
+                with open('output.txt', 'a') as file:
+                    file.write(f'Iteration {iteration}: {element_text}\n')
+
+            except Exception as e:
+                # Log the exception and continue with the next iteration
+                print(f'Error in iteration {iteration}, language {language}, city {city}: {str(e)}')
+
+finally:
+    driver.quit()
+
+citiesLangData_json = json.dumps(citiesLangData, indent=2)
+file_path = "citiesLangData.js"
+with open(file_path, "w") as file:
+    file.write(f"const citiesLangData = {citiesLangData_json};")
+
+print(f"City coordinates data has been saved to {file_path}")
+print(f'Successful iterations: {successful_iterations}')
