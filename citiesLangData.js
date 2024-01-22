7ea3d9a1a8f82fd657cb50465f1b8ab8c8f5d4ee
artdian <46709199+jartdian@users.noreply.github.com>
2024-01-22 20:57:26 +0100

    update cities lang data

---
+++ citiesLangData.js
@@ -0,0 +1,382 @@
+const citiesLangData = {
+  "Rotterdam": {
+    "coordinates": [
+      51.9225,
+      4.47917
+    ],
+    "data": {
+      "javascript": 634,
+      "python": 634,
+      "ruby": 36,
+      "java": 515,
+      "C%2B%2B": 203,
+      "c%23": 572,
+      "golang": 37,
+      "swift": 66,
+      "php": 505,
+      "sql": 872,
+      "typescript": 236,
+      "kotlin": 65
+    }
+  },
+  "Amsterdam": {
+    "coordinates": [
+      52.3676,
+      4.9041
+    ],
+    "data": {
+      "javascript": 692,
+      "python": 843,
+      "ruby": 80,
+      "java": 937,
+      "C%2B%2B": 278,
+      "c%23": 514,
+      "golang": 192,
+      "swift": 129,
+      "php": 397,
+      "sql": 1191,
+      "typescript": 442,
+      "kotlin": 227
+    }
+  },
+  "Den Haag": {
+    "coordinates": [
+      52.0705,
+      4.3007
+    ],
+    "data": {
+      "javascript": 583,
+      "python": 616,
+      "ruby": 38,
+      "java": 469,
+      "C%2B%2B": 177,
+      "c%23": 495,
+      "golang": 32,
+      "swift": 84,
+      "php": 466,
+      "sql": 787,
+      "typescript": 221,
+      "kotlin": 60
+    }
+  },
+  "Utrecht": {
+    "coordinates": [
+      52.0907,
+      5.1214
+    ],
+    "data": {
+      "javascript": 591,
+      "python": 539,
+      "ruby": 19,
+      "java": 743,
+      "C%2B%2B": 110,
+      "c%23": 474,
+      "golang": 40,
+      "swift": 48,
+      "php": 343,
+      "sql": 913,
+      "typescript": 311,
+      "kotlin": 164
+    }
+  },
+  "Eindhoven": {
+    "coordinates": [
+      51.4416,
+      5.4697
+    ],
+    "data": {
+      "javascript": 221,
+      "python": 214,
+      "ruby": 13,
+      "java": 197,
+      "C%2B%2B": 273,
+      "c%23": 283,
+      "golang": 19,
+      "swift": 29,
+      "php": 139,
+      "sql": 360,
+      "typescript": 72,
+      "kotlin": 31
+    }
+  },
+  "Groningen": {
+    "coordinates": [
+      53.2194,
+      6.5665
+    ],
+    "data": {
+      "javascript": 120,
+      "python": 62,
+      "ruby": 10,
+      "java": 109,
+      "C%2B%2B": 29,
+      "c%23": 61,
+      "golang": 2,
+      "swift": 6,
+      "php": 57,
+      "sql": 89,
+      "typescript": 46,
+      "kotlin": 6
+    }
+  },
+  "Zwolle": {
+    "coordinates": [
+      52.515,
+      6.0835
+    ],
+    "data": {
+      "javascript": 95,
+      "python": 38,
+      "ruby": 0,
+      "java": 41,
+      "C%2B%2B": 34,
+      "c%23": 82,
+      "golang": 3,
+      "swift": 4,
+      "php": 70,
+      "sql": 114,
+      "typescript": 26,
+      "kotlin": 4
+    }
+  },
+  "Nijmegen": {
+    "coordinates": [
+      51.8426,
+      5.8546
+    ],
+    "data": {
+      "javascript": 161,
+      "python": 105,
+      "ruby": 3,
+      "java": 98,
+      "C%2B%2B": 58,
+      "c%23": 139,
+      "golang": 2,
+      "swift": 23,
+      "php": 108,
+      "sql": 176,
+      "typescript": 73,
+      "kotlin": 14
+    }
+  },
+  "Tilburg": {
+    "coordinates": [
+      51.5719,
+      5.0672
+    ],
+    "data": {
+      "javascript": 231,
+      "python": 125,
+      "ruby": 9,
+      "java": 140,
+      "C%2B%2B": 81,
+      "c%23": 279,
+      "golang": 8,
+      "swift": 46,
+      "php": 209,
+      "sql": 388,
+      "typescript": 81,
+      "kotlin": 20
+    }
+  },
+  "'s-Hertogenbosch": {
+    "coordinates": [
+      51.6888,
+      5.2884
+    ],
+    "data": {
+      "javascript": 218,
+      "python": 132,
+      "ruby": 17,
+      "java": 147,
+      "C%2B%2B": 87,
+      "c%23": 285,
+      "golang": 4,
+      "swift": 44,
+      "php": 179,
+      "sql": 386,
+      "typescript": 74,
+      "kotlin": 24
+    }
+  },
+  "Breda": {
+    "coordinates": [
+      51.5831,
+      4.7484
+    ],
+    "data": {
+      "javascript": 135,
+      "python": 55,
+      "ruby": 3,
+      "java": 50,
+      "C%2B%2B": 38,
+      "c%23": 177,
+      "golang": 6,
+      "swift": 26,
+      "php": 160,
+      "sql": 225,
+      "typescript": 40,
+      "kotlin": 5
+    }
+  },
+  "Almere": {
+    "coordinates": [
+      52.3508,
+      5.2647
+    ],
+    "data": {
+      "javascript": 566,
+      "python": 675,
+      "ruby": 72,
+      "java": 798,
+      "C%2B%2B": 236,
+      "c%23": 432,
+      "golang": 192,
+      "swift": 106,
+      "php": 333,
+      "sql": 962,
+      "typescript": 395,
+      "kotlin": 198
+    }
+  },
+  "Apeldoorn": {
+    "coordinates": [
+      52.2112,
+      5.9699
+    ],
+    "data": {
+      "javascript": 83,
+      "python": 77,
+      "ruby": 4,
+      "java": 144,
+      "C%2B%2B": 36,
+      "c%23": 76,
+      "golang": 3,
+      "swift": 10,
+      "php": 59,
+      "sql": 153,
+      "typescript": 31,
+      "kotlin": 7
+    }
+  },
+  "Arnhem": {
+    "coordinates": [
+      51.9851,
+      5.8987
+    ],
+    "data": {
+      "javascript": 202,
+      "python": 132,
+      "ruby": 4,
+      "java": 109,
+      "C%2B%2B": 67,
+      "c%23": 180,
+      "golang": 2,
+      "swift": 20,
+      "php": 115,
+      "sql": 238,
+      "typescript": 96,
+      "kotlin": 19
+    }
+  },
+  "Haarlem": {
+    "coordinates": [
+      52.3874,
+      4.9024
+    ],
+    "data": {
+      "javascript": 637,
+      "python": 779,
+      "ruby": 77,
+      "java": 863,
+      "C%2B%2B": 265,
+      "c%23": 447,
+      "golang": 188,
+      "swift": 130,
+      "php": 368,
+      "sql": 1064,
+      "typescript": 410,
+      "kotlin": 216
+    }
+  },
+  "Enschede": {
+    "coordinates": [
+      52.2215,
+      6.8937
+    ],
+    "data": {
+      "javascript": 77,
+      "python": 77,
+      "ruby": 13,
+      "java": 132,
+      "C%2B%2B": 86,
+      "c%23": 81,
+      "golang": 2,
+      "swift": 7,
+      "php": 90,
+      "sql": 103,
+      "typescript": 28,
+      "kotlin": 11
+    }
+  },
+  "Amersfoort": {
+    "coordinates": [
+      52.1561,
+      5.3878
+    ],
+    "data": {
+      "javascript": 592,
+      "python": 531,
+      "ruby": 18,
+      "java": 690,
+      "C%2B%2B": 118,
+      "c%23": 486,
+      "golang": 50,
+      "swift": 48,
+      "php": 374,
+      "sql": 914,
+      "typescript": 327,
+      "kotlin": 168
+    }
+  },
+  "Leeuwarden": {
+    "coordinates": [
+      53.2194,
+      5.8054
+    ],
+    "data": {
+      "javascript": 26,
+      "python": 7,
+      "ruby": 0,
+      "java": 17,
+      "C%2B%2B": 6,
+      "c%23": 26,
+      "golang": 2,
+      "swift": 6,
+      "php": 24,
+      "sql": 20,
+      "typescript": 11,
+      "kotlin": 2
+    }
+  },
+  "Sittard": {
+    "coordinates": [
+      51.0018,
+      5.8662
+    ],
+    "data": {
+      "javascript": 62,
+      "python": 28,
+      "ruby": 0,
+      "java": 28,
+      "C%2B%2B": 5,
+      "c%23": 42,
+      "golang": 2,
+      "swift": 6,
+      "php": 41,
+      "sql": 58,
+      "typescript": 13,
+      "kotlin": 9
+    }
+  }
+};
